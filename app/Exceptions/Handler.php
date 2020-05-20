1f8dbdcffcb5ddb1cac9f9042fe6a98950b427e8
Roos-Skywalker <grifioen@live.nl>
2020-05-20 11:31:45 +0200

    Fix dependency issue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -17,7 +17,7 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * A list of the inputs that are never flashed for validation exceptions.
+     * A list of the inputs thate never flashed for validation exceptions.
      *
      * @var array
      */
