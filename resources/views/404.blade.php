c10d52dc5dceed818b97cc0430665ad08acbe400
Roos-Skywalker <grifioen@live.nl>
2020-06-03 19:02:37 +0200

    Added custom 404 page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+    <main>
+        <h1>Oeps! Deze pagina konden ik niet vinden</h1>
+        <span>
+            <h2>404</h2>
+        </span>
+    </main>
+@endsection
