173701c1dfe8b698245b2d6b16efa9bb29a22514
Roos-Skywalker <grifioen@live.nl>
2021-03-02 16:41:49 +0100

    Combined Laravel layout with custom layout for consistency

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -8,19 +8,11 @@
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="/css/style.css">
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
             .full-height {
                 height: 100vh;
             }
@@ -49,6 +41,16 @@
         </style>
     </head>
     <body>
+    <script type="text/javascript" src="/js/javascript.js"></script>
+    <ul class="navbar">
+        <li class="nav {{Request::path() === '/' ? 'active' : ''}}"><a href="/">Home</a></li>
+        <li class="nav {{Request::path() === 'profile' ? 'active' : ''}}"><a href="/profile">Profiel pagina</a></li>
+        <li class="nav {{Request::path() === 'dashboard' ? 'active' : ''}}"><a href="/dashboard">Dashboard</a></li>
+        <li class="nav {{Request::path() === 'motivatie' ? 'active' : ''}}"><a href="/motivatie">Motivatie</a></li>
+        <li class="nav {{Request::path() === 'beroepsbeeld' ? 'active' : ''}}"><a href="/beroepsbeeld">Beroepsbeeld</a></li>
+        <li class="nav {{Request::path() === 'construction' ? 'active' : ''}}"><a href="/construction">Curriculum Vitea</a></li>
+        <li class="nav {{Request::path() === 'blog' ? 'active' : ''}}"><a href="/blog">Blog</a></li>
+    </ul>
         <div class="flex-center position-ref full-height">
             <div class="code">
                 @yield('code')
