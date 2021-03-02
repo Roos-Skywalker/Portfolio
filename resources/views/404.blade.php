07c8cc8c15df5203a3c53cd1f4f57c068bd25e39
Roos-Skywalker <grifioen@live.nl>
2021-03-02 15:30:21 +0100

    deleted old 404

--- resources/views/404.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('content')
-    <main>
-        <h1>Oeps! Deze pagina konden ik niet vinden</h1>
-        <span>
-            <h2>404</h2>
-        </span>
-    </main>
-@endsection
