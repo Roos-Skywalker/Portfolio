ba7e574bad056625bdb7efc7f4c3285fc03ce747
Roos-Skywalker <grifioen@live.nl>
2021-04-14 13:48:40 +0200

    Changed 503 error message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,4 +4,7 @@
 @section('code', '503')
 @section('message', __($exception->getMessage() ?: 'Service Unavailable'))
 @section('details')
+    <p>Sorry, the site is temporarily down for maintenance or
+        there are too many visitors at the same time.
+        Try visiting us later.</p>
 @endsection
