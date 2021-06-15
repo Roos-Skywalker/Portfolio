27a81be6ee54c93ff1c8ed404590cf387ab80945
Roos-Skywalker <grifioen@live.nl>
2021-06-15 22:55:09 +0200

    AssignmentSeeder again

--- database/seeds/AssignmentSeeder.php
+++ database/seeds/AssignmentSeeder.php
@@ -2,18 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Assignment;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AssignmentSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(AssignmentSeeder::class);
+        Assignment::factory()->count(5)->create();
     }
 }
