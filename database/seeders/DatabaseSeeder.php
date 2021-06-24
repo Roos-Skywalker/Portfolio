71a69cb82b078f528d588b7dde9dd3fef9c83d37
Roos-Skywalker <grifioen@live.nl>
2021-06-24 15:15:19 +0200

    can now seed Users also in production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(AssignmentSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
